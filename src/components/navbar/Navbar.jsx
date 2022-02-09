788e8320160139cbf404e233ded93668ceadf868
Howun Lam <howunlam96@gmail.com>
2022-02-09 13:13:39 +0000

    Setup

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Navbar = () => {
+  return <div className="navBar">
+    <p>Hello</p>
+  </div>;
+};
+
+export default Navbar;
---
+++ src/containers/profile/Profile.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Navbar from '../../components/navbar/Navbar';
+
+const Profile = () => {
+  return <>
+  
+  <Navbar className="navbar"/>
+  </>;
+};
+
+export default Profile;
